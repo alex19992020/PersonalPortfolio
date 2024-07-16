81fd977242f996c90269548d58cba7965b4e6f1e
alex19992020 <alejandroantunez1999@gmail.com>
2024-07-15 19:50:28 -0700

    finishing touches to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,11 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+
 const Navbar = () => {
-  return <nav className="bg-red-900 mb-20 flex intems-center justify-between py-6">
+  return <nav className="mb-20 flex intems-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="logo" />
+        <img className="mx-2 w-10" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
